1ba710e9377930cc2344e6be738f9b26f0ba68cb
Nicolas Jorand <nicolas.jorand@me.com>
2021-01-31 17:58:02 +0100

    Spawn the alien

--- Scripts/Area_baggage.gd
+++ Scripts/Area_baggage.gd
@@ -6,11 +6,33 @@ extends Area
 # var b = "text"
 var score = null
 export var exitNb = 1
+var currentColors = []
+var existingColors = ["Gris", "Jaune","Mauve","Turquois","Vert"]
+var existingType = ["Ufo","Lug"]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
   score = get_tree().root.get_node("Main/Score") # Replace with function body.
 
 func _on_Area_body_entered(body):
+  if body.visible == false:
+    print("Body not visible %s" % body.name)
+    pass
+  if !(body is  RigidBody):
+    pass
+  var isLug = false
+  for tag in body.get_groups():
+    if (tag in existingType) and (tag == "Lug"):
+      isLug = true
+      print("Got Luggage")
+    elif (tag in existingType) and (tag == "Ufo"):
+      print("Got Ufo Body")
+      for t in body.get_groups():
+        if t in existingColors:
+          print("Ufo and color ...")
+          currentColors.append(t)
+          print(currentColors)
+        
   if body is RigidBody && "Blue" in body.name:
     print("Luggage '%s' Match" % body.name)
     body.queue_free()
