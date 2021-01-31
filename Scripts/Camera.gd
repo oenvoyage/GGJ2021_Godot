b7a3f9035791ece91dd1f6d1d7ca4143c28ac127
Olivier Amrein <oenvoyage@gmail.com>
2021-01-31 16:06:43 +0100

    changes to camera etc

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -2,7 +2,7 @@ extends Camera
 
 
 # Declare member variables here. Examples:
-# var a = 2
+var isCameraInHall = true
 # var b = "text"
 
 
@@ -10,6 +10,17 @@ extends Camera
 func _ready():
 	pass # Replace with function body.
 
+func _input(event):
+	# if event is InputEventKey and event.scancode == KEY_C:
+	if event.is_action_pressed("ui_home"):
+		if isCameraInHall == true:
+			$AnimationPlayer.play("hall_to_belt")
+			isCameraInHall = false
+		
+		else:
+			$AnimationPlayer.play_backwards("hall_to_belt")
+			isCameraInHall = true
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
