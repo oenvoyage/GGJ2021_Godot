11d7195e958658c7389c8e590399073007198ced
Nicolas Jorand <nicolas.jorand@me.com>
2021-01-31 12:16:07 +0100

    add score

---
+++ Scripts/Score.gd
@@ -0,0 +1,23 @@
+extends Spatial
+
+var score = 0
+var maxScore = 30
+var scoreNode = null
+var maxNode = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  score = 0
+  scoreNode = get_node("../HUD/Score/Current")
+  maxNode = get_node("../HUD/Score/Max")
+  
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  scoreNode.text = score as String
+  maxNode.text = maxScore as String
+
+
+func _on_Timer_timeout():
+  score += 1
