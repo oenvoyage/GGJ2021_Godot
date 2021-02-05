716982b76139de6c5bf58e2b33791be8e39742cb
Nicolas Jorand <nicolas.jorand@me.com>
2021-02-06 00:45:19 +0100

    Add the key 1,2,3 to move the switches

--- Scripts/Controller.gd
+++ Scripts/Controller.gd
@@ -1,9 +1,10 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var switch_1 = get_node("Switch_S_1")
+onready var switch_2 = get_node("Switch_S_2")
+onready var switch_3 = get_node("Switch_S_3")
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,6 +14,13 @@ func _ready():
   get_node("Curve_NW_Back/curve_belt_elements").speed_set(-Universe.conveyor_speed)
   get_node("Curve_NW_Front/curve_belt_elements").speed_set(-Universe.conveyor_speed)
   
+func _input(event):
+  if event.is_action_pressed("switch1"):
+    switch_1.switch()
+  elif event.is_action_pressed("switch2"):
+    switch_2.switch()
+  elif event.is_action_pressed("switch3"):
+    switch_3.switch()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
