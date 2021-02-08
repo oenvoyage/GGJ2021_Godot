de2b9aa8cf25399c4911c885331d5ae193e26726
Nicolas Jorand <nicolas.jorand@me.com>
2021-02-08 23:28:04 +0100

    Add the animation

--- Prefabs/UFOSpawner.gd
+++ Prefabs/UFOSpawner.gd
@@ -3,6 +3,7 @@ extends Spatial
 export var spawnerId = 1
 var ufos = []
 var currentUfo = null
+var rng = RandomNumberGenerator.new()
 
 func pop():
   if currentUfo != null:
@@ -12,6 +13,17 @@ func pop():
   currentUfo.translation = $Spawner.translation
   currentUfo.set_mode(RigidBody.MODE_RIGID)
   currentUfo.visible = true
+  var animation = currentUfo.get_node("Alien/AnimationPlayer") 
+  var animList = animation.get_animation_list()
+  for animName in animList:
+    if "Circle_animation" in  animName:
+        animation.get_animation(animName).set_loop(false)
+        animation.stop()
+    elif "tictac_animation" in  animName:
+        var anim = animation.get_animation(animName)
+        anim.set_loop(true)
+        animation.set_speed_scale(rng.randi_range(5,20))
+        animation.play(animName)
   currentUfo.add_to_group("Spawner%s" % spawnerId)
   $Spawner.add_child(currentUfo)
   print_debug("Spawner %s spawning %s" % [spawnerId, currentUfo.name])
